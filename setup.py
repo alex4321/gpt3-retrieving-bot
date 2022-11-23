2ff11e63bb8445515d1280a05c7dd517aaf0c358
Alex Pozharskii <alexpozharskii@epoch8.co>
2022-11-23 09:26:13 +0300

    Package naming

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), "requirements.txt"), "r", enco
 with open(os.path.join(os.path.dirname(__file__), "README.md"), "r", encoding="utf-8") as readme:
     readme_text = readme.read()
 setup(
-    name="Augmented LM Chatbots",
+    name="aulm-chatbots",
     version="0.0.1",
     packages=find_packages(include=["chatbots", "chatbots.*"]),
     install_requires=requirements,
@@ -15,5 +15,5 @@ setup(
     author_email="gaussmake@gmail.com",
     url="https://github.com/alex4321/gpt3-retrieving-bot",
     long_description=readme,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
 )
